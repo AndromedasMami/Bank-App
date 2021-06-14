1b7a200255c9735216edd43721121552e0ed50bf
Melanie Miguel Villavicencio <80484383+AndromedasMami@users.noreply.github.com>
2021-06-13 21:39:32 -0400

    Added api folder.

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const routes = {
   '/login': { 
-    templateId: 'login' 
+    templateId: 'login',
+    title: 'Login'
   },
   '/dashboard': { 
-    templateId: 'dashboard' 
+    templateId: 'dashboard',
+    title: 'Dashboard',
+    init: refresh
   },
 };
+
+const storageKey = 'savedAccount';
+
+let state = Object.freeze({
+  account: null
+});
+
+function updateState(property, newData) {
+  state = Object.freeze({
+    ...state,
+    [property]: newData
+  });
+
+  localStorage.setItem(storageKey, JSON.stringify(state.account));
+}
+
+function updateElement(id, textOrNode) {
+  const element = document.getElementById(id);
+  element.textContent = '';
+  element.append(textOrNode);
+}
 
 function updateRoute() {
   const path = window.location.pathname;
@@ -22,6 +46,12 @@ function updateRoute() {
   const app = document.getElementById('app');
   app.innerHTML = '';
   app.appendChild(view);
+
+  if (typeof route.init === 'function') {
+    route.init();
+  }
+
+  document.title = route.title;
 }
 
 function navigate(path) {
@@ -34,8 +64,48 @@ function onLinkClick(event) {
   navigate(event.target.href);
 }
 
-window.onpopstate = () => updateRoute();
-updateRoute();
+async function refresh() {
+  await updateAccountData();
+  updateDashboard();
+}
+
+async function login() {
+  const loginForm = document.getElementById('loginForm')
+  const user = loginForm.user.value;
+  const data = await getAccount(user);
+
+  if (data.error) {
+    return updateElement('loginError', data.error);
+  }
+
+  updateState('account', data);
+  navigate('/dashboard');
+  updateDashboard();
+}
+
+async function getAccount(user) {
+  try {
+    const response = await fetch('//localhost:5000/api/accounts/' + encodeURIComponent(user));
+    return await response.json();
+  } catch (error) {
+    return { error: error.message || 'Unknown error' };
+  }
+}
+
+async function updateAccountData() {
+  const account = state.account;
+  if (!account) {
+    return logout();
+  }
+
+  const data = await getAccount(account.user);
+  if (data.error) {
+    return logout();
+  }
+
+  updateState('account', data);
+}
+
 
 async function register() {
   const registerForm = document.getElementById('registerForm');
@@ -49,7 +119,10 @@ async function register() {
     return console.log('An error occured:', result.error);
   }
 
+  updateState('account', result);
   console.log('Account created!', result);
+  navigate('/dashboard');
+  updateDashboard();
 }
 
 async function createAccount(account) {
@@ -64,3 +137,51 @@ async function createAccount(account) {
     return { error: error.message || 'Unknown error' };
   }
 }
+
+function updateDashboard() {
+  const account = state.account;
+  console.log("update dashboard: " + account);
+  console.log("Dashboard is shown");
+  if (!account) {
+    return logout();
+  }
+
+  updateElement('description', account.description);
+  updateElement('balance', account.balance.toFixed(2));
+  updateElement('currency', account.currency);
+
+  const transactionsRows = document.createDocumentFragment();
+  for (const transaction of account.transactions) {
+    const transactionRow = createTransactionRow(transaction);
+    transactionsRows.appendChild(transactionRow);
+  }
+  updateElement('transactions', transactionsRows);
+}
+
+function createTransactionRow(transaction) {
+  const template = document.getElementById('transaction');
+  const transactionRow = template.content.cloneNode(true);
+  const tr = transactionRow.querySelector('tr');
+  tr.children[0].textContent = transaction.date;
+  tr.children[1].textContent = transaction.object;
+  tr.children[2].textContent = transaction.amount.toFixed(2);
+  return transactionRow;
+}
+
+function logout() {
+  updateState('account', null);
+  navigate('/login');
+}
+
+function init() {
+  const savedAccount = localStorage.getItem(storageKey);
+
+  if (savedAccount) {
+    updateState('account', JSON.parse(savedAccount));
+  }
+
+  window.onpopstate = () => updateRoute();
+  updateRoute();
+}
+
+init();
